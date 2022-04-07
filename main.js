5a80e3fb04e0fda83310d228bede2f8ada4039da
Random-Person100 <91764794+Random-Person100@users.noreply.github.com>
2022-04-06 17:42:24 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function take_snapshot(){
     Webcam.snap(function(data_uri){document.getElementById("result").innerHTML=
 '<img id="captured_image" src="'+data_uri+'">'})
 }
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/l9lz4my84/model.json',modelloaded)
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelloaded)
 function modelloaded(){
     console.log("model loaded")
 }
@@ -22,4 +22,45 @@ function speak(){
     speak_data2="first prediction is "+prediction_2
     var utterThis=new SpeechSynthesisUtterance(speak_data1+speak_data2);
     synth.speak(utterThis)
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        document.getElementById("result_emotion_name").innerHTML=prediction_1;
+        document.getElementById("result_emotion_name2").innerHTML=prediction_2;
+        speak()
+        if(results[0].label=="amazing"){
+            document.getElementById("update_emoji").innerHTML="&#128076;";
+
+        }
+        if(results[0].label=="best"){
+            document.getElementById("update_emoji").innerHTML="&#128077;";
+
+        }
+        if(results[0].label=="victory"){
+            document.getElementById("update_emoji").innerHTML="&#9996;";
+
+        }
+        if(results[1].label=="amazing"){
+            document.getElementById("update_emoji2").innerHTML="&#128076;";
+
+        }
+        if(results[1].label=="best"){
+            document.getElementById("update_emoji2").innerHTML="&#128077;";
+
+        }
+        if(results[1].label=="victory"){
+            document.getElementById("update_emoji2").innerHTML="&#9996;";
+
+        }
+    }
 }
